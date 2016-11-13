403bb0c5e3be5f56c014ed24ecb668bff0c0f41a
eldooon <eldonchan91@gmail.com>
2016-11-13 12:33:44 -0500

    Fixed storyboard minor issue

---
+++ inYC/inYCExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  inYCExtensions.swift
+//  inYC
+//
+//  Created by Eldon Chan on 11/13/16.
+//  Copyright © 2016 Eldon. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+
+    class func inYCNormal() -> UIFont {
+        return UIFont(name: "TrebuchetMS", size: 18)!
+    }
+    
+    class func inYCBold() -> UIFont {
+        return UIFont(name: "TrebuchetMS-Bold", size: 18)!
+    }
+
+}
+
+extension UIColor {
+
+    
+}
