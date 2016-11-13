065c5c9bbc61829e337a4728e8fb7d58595376e9
eldooon <eldonchan91@gmail.com>
2016-11-13 11:15:26 -0500

    fixed tint color

--- inYC/AppDelegate.swift
+++ inYC/AppDelegate.swift
@@ -17,7 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.\
         
-        UITabBar.appearance().tintColor = UIColor.black
+//        UITabBar.appearance().tintColor = UIColor.black
+//        UITabBarItem.appearance().image.tint
+//        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.black], for: .normal)
+        
         return true
     }
 
